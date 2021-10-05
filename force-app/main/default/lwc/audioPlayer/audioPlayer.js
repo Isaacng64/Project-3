4f4f1b70f9381e0ef2790628fe100489216a4824
IndustrialDonut <76419762+IndustrialDonut@users.noreply.github.com>
2021-10-05 14:16:53 -0700

    object of options including volume and name to play

--- force-app/main/default/lwc/audioPlayer/audioPlayer.js
+++ force-app/main/default/lwc/audioPlayer/audioPlayer.js
@@ -6,35 +6,33 @@ import dosSongs from '@salesforce/resourceUrl/dosSongs';
 
 export default class AudioPlayer extends LightningElement {
 
-        //a = new Audio(dosSongs + '/audioSmall/s1.mp3');
-        //b = new Audio(dosSongs + '/audioSmall/s2.mp3');
-
     volume;
     changeVolume(event){
         this.volume = event.target.value;
-        console.log(this.volume);
     }
 
     playHandle(){
-        this.play(this.volume);
+        this.play({volume: this.volume, name: "s1"});
     }
 
-    play(volume){
-        let a = new Audio(dosSongs + '/audioSmall/s1.mp3');
-        a.volume = volume;
+    play(opts){
+
+        if(opts["name"]){
+            var a = new Audio(dosSongs + '/audioSmall/' + opts["name"] + '.mp3');
+        }else{
+            var a = new Audio(dosSongs + '/audioSmall/s1.mp3'); // default song to play
+        }
+
+        if(opts["volume"]){
+            a.volume = opts["volume"];
+        }else{
+            a.volume = 0.5;
+        }
+        
         a.play();
+    }
 
-            //console.log('playing ' + dosSongs + '/audioSmall/s1.mp3');
-            //this.a.play();
-            //this.b.play();
-    }
-         
-    /*
-    dec(){
-        this.a.volume -= 0.1;
-        this.b.volume -= 0.1;
-    }
-         
+    /*   
     playLink(){
         let a = new Audio('https://srv3.onlymp3.net/download?file=3a8ae414bc7e7955109768889a4f1f9d251003003');
         a.play();
