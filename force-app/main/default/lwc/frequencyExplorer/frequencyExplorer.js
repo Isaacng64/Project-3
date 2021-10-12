0d303e802f680b3aea4e09f38e4ba9f0484ff5b0
Andrew Wensley <awensley@u.rochester.edu>
2021-10-12 15:29:45 -0400

    Added note index inut to GuitarTonePlayer, not index functionality to frequencyExplorer

--- force-app/main/default/lwc/frequencyExplorer/frequencyExplorer.js
+++ force-app/main/default/lwc/frequencyExplorer/frequencyExplorer.js
@@ -12,9 +12,18 @@ export default class FrequencyExplorer extends LightningElement {
 
     constructor(){
         super();
+        this.populatePlayers();
+    }
 
-        
-        this.players.push(new TonePlayer({name: "E", octave: 2}));
+    populatePlayers() {
+        this.names = [];
+        this.players = [];
+        let tempTone = new TonePlayer({name: "E", octave: 2})
+        //this.players.push(tempTone);
+        let tempFreq = tempTone.oscillator.frequency.value;
+        tempFreq *= Math.pow(2, this.noteIndex/12);
+        this.players.push(new TonePlayer({freq: (tempFreq)}));
+
         this.names.push(Math.trunc(this.players[0].oscillator.frequency.value) + " Hz");
 
         let fundamental = this.players[0].oscillator.frequency.value;
@@ -36,6 +45,7 @@ export default class FrequencyExplorer extends LightningElement {
         let dest = this.names.indexOf(event.target.label);
 
         this.players[dest].setVol(amp / 100.0);
+        
     }
 
     pulse(){
@@ -45,10 +55,31 @@ export default class FrequencyExplorer extends LightningElement {
     }
 
     a = new GuitarTonePlayer();
+    noteIndex = 0;
     guitar(){
         //this.a = new GuitarTonePlayer();
+        
+        //this.noteIndex += 1;
+        this.a = new GuitarTonePlayer(this.noteIndex);
         this.a.play();
         console.log(this.a);
+    }
+
+    stopGuitar() {
+        this.a.stop();
+    }
+
+    stopAll() {
+        for(let i = 0; i < this.names.length; i++){
+            this.players[i].stop();
+            //this.players[i].setVol(0);
+        }
+        this.stopGuitar();
+    }
+
+    updateNoteIndex() {
+        this.noteIndex = event.target.value;
+        this.populatePlayers();
     }
 
 }
