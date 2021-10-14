27b54c800bab9bea1c977d2cfad82d74075980dc
VicP <68759920+avpr2000@users.noreply.github.com>
2021-10-14 16:31:27 -0400

    Change with the Piano (Three Octaves) dynamic

---
+++ force-app/main/default/lwc/octaveLayout/octaveLayout.js
@@ -0,0 +1,7 @@
+import { LightningElement, api } from 'lwc';
+
+export default class OctaveLayout extends LightningElement {
+    // Component who shows the UI for the Octave
+    @api componentoctave;
+
+}
