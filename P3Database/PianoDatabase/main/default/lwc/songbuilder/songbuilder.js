b5baa83c885277a74b5909a006dc8b25846638b3
CohenJosh <josh8950@gmail.com>
2021-10-15 09:41:23 -0400

    added song player

--- P3Database/PianoDatabase/main/default/lwc/songbuilder/songbuilder.js
+++ P3Database/PianoDatabase/main/default/lwc/songbuilder/songbuilder.js
@@ -1,21 +1,61 @@
 import { LightningElement, track, api, wire } from 'lwc';
 import CreateMeasure from '@salesforce/apex/PianoController.CreateMeasure';
+import CreateSong from '@salesforce/apex/PianoController.CreateSong';
+import ClearSong from '@salesforce/apex/PianoController.ClearSong';
+import NameSong from '@salesforce/apex/PianoController.NameSong';
+import RetrieveSong from '@salesforce/apex/PianoController.LoadMusicNote';
+import GetAllSongs from '@salesforce/apex/PianoController.GetSongsByName';
 
 export default class songbuilder extends LightningElement {
   @track dataForDynamicComponentCreation = [];
   index = 0;
   @track octave = 4;
+  songID = '';
+  songName;
+  loadInputValue;
+  saveInputValue;
+  measureNotes = [];
+  playing = false;
+  loading = false;
+  saving = false;
+  @track listOfSongs = [];
+  @track songToPlay = [];
+  currNote = 0;
+
+  connectedCallback(){
+    CreateSong()
+        
+        .then((result) => {
+            this.songID = result + '';
+        })
+        
+        .catch((error) => {
+            this.error = error;
+        });
+  }
+
+  emptySong() {
+    ClearSong({ song: this.songID })
+        
+        .then((result) => {
+            console.log(result);
+        })
+        
+        .catch((error) => {
+            this.error = error;
+        });
+  }
 
   addNewMeasure() {
-    CreateMeasure()
-    
+    CreateMeasure({ song: this.songID, pos: this.index })
+
     .then((result) => {
-      console.log('measureId in parent: ' + result);
       this.dataForDynamicComponentCreation = [
         ...this.dataForDynamicComponentCreation,
         {
             requiredKey: this.index, 
-            measureid: (result + '')
+            measureid: (result + ''),
+            LoadedNotes: []
         }
       ];
     })
@@ -31,6 +71,22 @@ export default class songbuilder extends LightningElement {
     this.dataForDynamicComponentCreation = [
         ...this.dataForDynamicComponentCreation.slice(0, -1)
     ];
+
+    this.index--;
+  }
+
+  newSong() {
+    CreateSong()
+        
+      .then((result) => {
+          this.songID = result + '';
+          this.dataForDynamicComponentCreation = [];
+          this.index = 0;
+      })
+      
+      .catch((error) => {
+          this.error = error;
+      });
   }
   
   addOctave() {
@@ -60,14 +116,151 @@ export default class songbuilder extends LightningElement {
     }
     this.octave = cCells[0].innerHTML.substring(1) - 0;
   }
+  startPlaying() {
+    if (this.loading == true) {
+      this.listOfSongs = [];
+    }
+    this.loading = false;
+    this.saving = false;
+    this.playing = !this.playing;
+
+    this.songToPlay = [];
+
+    let measureCMPs = this.template.querySelectorAll('c-songbuildermeasure');
+
+    for (let i = 0; i < measureCMPs.length; i++){
+      let currMeasureNotes = measureCMPs[i].getNotesInMeasure();
+
+      this.songToPlay = this.songToPlay.concat(currMeasureNotes);
+    }
+  }
+
+  startSaving() {
+    if (this.loading == true) {
+      this.listOfSongs = [];
+    }
+    this.playing = false;
+    this.loading = false;
+    this.saving = !this.saving;
+  }
 
   saveSong() {
     let measureCMPs = this.template.querySelectorAll('c-songbuildermeasure');
-    console.log('measureCMPs length: ' + measureCMPs.length);
+    this.emptySong();
+    this.renameSong();
     for (let i = 0; i < measureCMPs.length; i++){
-      measureCMPs[i].addMeasureNotes();
+      measureCMPs[i].addMeasureNotes(this.songID);
+    }
+    alert('Your song has been saved!');
+    this.saving = false;
+  }
+
+  startLoading() {
+    if (this.loading == true) {
+      this.listOfSongs = [];
+    }
+    this.playing = false;
+    this.saving = false;
+    this.loading = !this.loading;
+  }
+
+  loadAllSongs(){
+    this.listOfSongs = [];
+    GetAllSongs({ song: this.loadInputValue })
+        
+      .then((result) => {
+          for (let i = 0; i < result.length; i++){
+            this.listOfSongs = [
+              ...this.listOfSongs,
+              {
+                  songName: result[i].substring(18),
+                  songID: result[i].substring(0,18)
+              }
+            ];
+          }
+      })
+      
+      .catch((error) => {
+          this.error = error;
+      });
+  }
+
+  loadSong(event) {
+
+    let loadingSongID = event.target.name;
+    RetrieveSong({ song: loadingSongID })
+        
+    .then((result) => {
+      this.songID = loadingSongID;
+      this.dataForDynamicComponentCreation = [];
+      this.index = 0;
+
+      for (let i = 0; i < result.length; i+=2){
+        let notesForMeasure = [];
+        for (let j = 0; j < result[i+1].length; j+=4){
+          notesForMeasure.push(result[i+1].substring(j, j + 4));
+        }
+        
+        this.dataForDynamicComponentCreation = [
+          ...this.dataForDynamicComponentCreation,
+          {
+              requiredKey: this.index, 
+              measureid: (result[i]),
+              LoadedNotes: notesForMeasure
+          }
+        ];
+
+        this.index++;
+      }
+
+      this.loading = false;
+    })
+    
+    .catch((error) => {
+        this.error = error;
+        if (error.body.message == 'List has no rows for assignment to SObject'){
+          alert('No song found with that ID.');
+        } else if (error.body.message == 'Argument cannot be null.') {
+          alert('Please enter a song name.')
+        }
+    });
+  }
+
+  renameSong() {
+    if (this.saveInputValue == ''){
+      this.saveInputValue = this.songID;
+    }
+    NameSong({ song: this.songID, newName: this.saveInputValue })
+        
+        .then((result) => {
+            console.log(result);
+        })
+        
+        .catch((error) => {
+            this.error = error;
+        });
+  }
+
+  handleTick(){
+    if (this.currNote == this.songToPlay.length) {
+      this.currNote = 0;
     }
 
-    alert('Your song has been saved!');
+    for (let i = 0; i < this.songToPlay[this.currNote].length; i+=2){
+      if (!(this.songToPlay[this.currNote] == '')) {
+        let note = this.songToPlay[this.currNote].substring(i,i+2) - 0;
+        this.template.querySelector("c-audio-player").playPianoInteger(note);
+      }
+    }
+
+    this.currNote++;
+  }
+
+  handleLoadInputChange(event){
+    this.loadInputValue = event.target.value;
+  }
+
+  handleSaveInputChange(event){
+    this.saveInputValue = event.target.value;
   }
 }
--- P3Database/PianoDatabase/main/default/lwc/songbuildermeasure/songbuildermeasure.js
+++ P3Database/PianoDatabase/main/default/lwc/songbuildermeasure/songbuildermeasure.js
@@ -1,47 +1,84 @@
 import { LightningElement, track, api, wire } from 'lwc';
-import UpdateMeasure from '@salesforce/apex/PianoController.UpdateMeasure';
 import SaveMusicNote from '@salesforce/apex/PianoController.SaveMusicNote';
 
 export default class songbuildermeasure extends LightningElement {
   @api measureId;
   @api octave;
+  @api songId;
+  @api loadedNotes;
   notesInMeasure = [];
+  
+  connectedCallback(){
+    let loadResult = JSON.stringify(this.loadedNotes);
+    let addResult = loadResult + 'a';
+    let loadedList = addResult.substring(1, loadResult.length-1).split(',');
+
+    for (let i = 0; i < loadedList.length; i++){
+      if (!(loadedList[i] == '')) {
+        this.notesInMeasure.push(loadedList[i].replaceAll('"', ''));
+      }
+    }
+  }
 
   renderedCallback() {
     let tds = this.template.querySelectorAll('td');
     var self = this;
-    console.log('measureId in measure: ' + this.measureId);
+    let octaveOffset = (self.octave - 3) * 12;
+    let picked = [];
+    for (let j = 0; j < this.notesInMeasure.length; j++){
+      let toPick = ((this.notesInMeasure[j] + '').substring(0,2) - octaveOffset).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false}) + '-' + (this.notesInMeasure[j] + '').substring(2);
+      picked.push(toPick);
+    }
     for (let i = 0; i < tds.length; i++) {
       tds[i].addEventListener('click', function(){
-        let octaveOffset = (self.octave - 3) * 12;
         if (this.style.background == 'green'){
           let toRemove = self.notesInMeasure.indexOf((this.classList[0].split('-')[0] - 0 + octaveOffset).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false}) + this.classList[0].split('-')[1]);
           if (toRemove != -1) {
             self.notesInMeasure.splice(toRemove, 1);
           }
           this.style.background = 'white';
-          console.log(self.notesInMeasure);
         } else {
-          console.log('current octave' + self.octave);
           self.notesInMeasure.push((this.classList[0].split('-')[0] - 0 + octaveOffset).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false}) + this.classList[0].split('-')[1]);
           this.style.background = 'green';
-          console.log(self.notesInMeasure);
         }
       }, false);
+      if (picked.includes(tds[i].classList[0])) {
+        tds[i].style.background = 'green';
+      }
     }
   }
 
-  @api addMeasureNotes() {
-    console.log('called child addMeasureNotes');
-    SaveMusicNote({ note: this.notesInMeasure, stringId: this.measureId })
+  @api getNotesInMeasure(){
+    let notes = [];
+    for (let i = 0; i < 16; i++){
+      notes[i] = '';
+    }
+    for (let i = 0; i < this.notesInMeasure.length; i++){
+      let currPosition = this.notesInMeasure[i].substring(2) - 0;
+      let currPitch = this.notesInMeasure[i].substring(0,2);
+
+      notes[currPosition] += currPitch;
+
+    }
+    return notes;
+  }
+
+  @api addMeasureNotes(songID) {
+    SaveMusicNote({ note: this.notesInMeasure, stringId: this.measureId, song: songID})
         
-        .then((result) => {
-            console.log(result);
-        })
-        
-        .catch((error) => {
-            this.error = error;
-        });
+    .then((result) => {
+        console.log(result);
+    })
+    
+    .catch((error) => {
+        this.error = error;
+    });
+  }
+
+  @api addLoadedNotes(){
+    this.notesInMeasure = [];
+    this.notesInMeasure = this.loadedNotes;
+    this.handleNotesChange();
   }
 
   @api get currOctave() {
@@ -51,17 +88,16 @@ export default class songbuildermeasure extends LightningElement {
   set currOctave(value) {
     this.setAttribute('currOctave', value);
     this.octave = value;
-    this.handleValueChange(value);
+    this.handleNotesChange();
   }
 
-  handleValueChange(value) {
+  handleNotesChange() {
     let tds = this.template.querySelectorAll('td');
     let octaveOffset = (this.octave - 3) * 12;
     let picked = [];
     for (let j = 0; j < this.notesInMeasure.length; j++){
       let toPick = ((this.notesInMeasure[j] + '').substring(0,2) - octaveOffset).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false}) + '-' + (this.notesInMeasure[j] + '').substring(2);
-      picked.push(toPick);
-      console.log(picked);
+      picked.push(toPick);w
     }
     for (let i = 0; i < tds.length; i++){
       tds[i].style.background = 'white';
