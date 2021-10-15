ecc457ee9de7e2870f921dc719aef61fcac8f2c9
IndustrialDonut <76419762+IndustrialDonut@users.noreply.github.com>
2021-10-15 12:32:23 -0700

    strange formatting but fixed now

--- AudioPlayerPack/main/default/lwc/audioPlayer/audioPlayer.js
+++ AudioPlayerPack/main/default/lwc/audioPlayer/audioPlayer.js
@@ -40,66 +40,41 @@ export default class AudioPlayer extends LightningElement {
   }
 
   @api
-  tickCallback(tickVolume, autoNotes) {
-    this.metronomePlayer.currentTime = 0.0;
-    this.metronomePlayer.volume = tickVolume;
-    this.metronomePlayer.play();
+  tickCallback(tickVolume, autoNotes){
 
-    for (let i = 0; i < this.currentlyPlaying.length; i++) {
-      this.autoPlaying[i].stop();
-    }
-
-    /* Thinking ahead you may optionally take params for duration and volume IN HERE separate from note construction which is just THE NOTE */
-    @api
-    playPianoInteger(i){ 
-        this.playPiano(new AudioPlayerNote(i));
-    }
-
-    @api
-    playPiano(playerNote){
-        this.play(playerNote, "piano");
-    }   
-
-    @api
-    tickCallback(tickVolume, autoNotes){
-
-        this.metronomePlayer.currentTime = 0.0;
-        this.metronomePlayer.volume = tickVolume;
-        this.metronomePlayer.play();
+      this.metronomePlayer.currentTime = 0.0;
+      this.metronomePlayer.volume = tickVolume;
+      this.metronomePlayer.play();
 
 
-        for(let i = 0; i < this.autoPlaying.length; i++){
-            this.autoPlaying[i].stop();
-        }
-        this.autoPlaying = [];
+      for(let i = 0; i < this.autoPlaying.length; i++){
+          this.autoPlaying[i].stop();
+      }
+      this.autoPlaying = [];
 
-        for(let i = 0; i < autoNotes.length; i++){
-            this.playPiano(autoNotes[i]);
-            this.autoPlaying.push(this.clientNotesAuto["piano"][autoNotes[i].octave][autoNotes[i].name]);
-        }
-    }
+      for(let i = 0; i < autoNotes.length; i++){
+          this.playPiano(autoNotes[i]);
+          this.autoPlaying.push(this.clientNotesAuto["piano"][autoNotes[i].octave][autoNotes[i].name]);
+      }
+  }
 
-    play(playerNote, instrument){
+  play(playerNote, instrument){
 
-        if(playerNote.valid){
-            this.clientNotesAuto[instrument][playerNote.octave][playerNote.name].play(); // can specify duration in player.play() function!
-            this.clientNotesAuto[instrument][playerNote.octave][playerNote.name].setVolume(this.volume);
-        }else{
-            throw "no valid note to play!";
-        }
+      if(playerNote.valid){
+          this.clientNotesAuto[instrument][playerNote.octave][playerNote.name].play(); // can specify duration in player.play() function!
+          this.clientNotesAuto[instrument][playerNote.octave][playerNote.name].setVolume(this.volume);
+      }else{
+          throw "no valid note to play!";
+      }
 
-        this.currentlyPlaying.push(this.clientNotesAuto[instrument][playerNote.octave][playerNote.name]);
-    }
+      this.currentlyPlaying.push(this.clientNotesAuto[instrument][playerNote.octave][playerNote.name]);
+  }
 
-    changeVolume(event){
-        this.volume = event.target.value;
-        for(let i = 0; i < this.currentlyPlaying.length; i++){
-            this.currentlyPlaying[i].setVolume(this.volume);
-        }
-    }
-
-  changeVolume(event) {
-    this.volume = event.target.value;
+  changeVolume(event){
+      this.volume = event.target.value;
+      for(let i = 0; i < this.currentlyPlaying.length; i++){
+          this.currentlyPlaying[i].setVolume(this.volume);
+      }
   }
 }
 
