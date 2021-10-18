5744e9350f6161b2a84c2a3fd7d99596676689d2
John Bush <69868497+TruettBush@users.noreply.github.com>
2021-10-18 15:17:15 -0500

    flipped tuning

--- StringPackageFolder/lwc/guitarInstrument/guitarInstrument.js
+++ StringPackageFolder/lwc/guitarInstrument/guitarInstrument.js
@@ -12,14 +12,14 @@ export default class GuitarInstrument extends LightningElement {
   currentFret;
   stringNumber;
 
-  tuning = ['E1', 'A', 'D', 'G', 'B', 'E2'];
+  tuning = ['E2', 'B', 'G', 'D', 'A', 'E1'];
 
   currentFretAndString(event) {
     let result = event.detail;
     this.currentFret = result[0];
     this.stringNumber = result[1];
   }
-  
+
   playGuitar(event) {
     let note = event.detail;
     this.template.querySelector('c-audio-player').playGuitar(note[0], note[1]);
