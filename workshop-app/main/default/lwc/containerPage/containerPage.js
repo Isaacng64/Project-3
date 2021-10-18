0dec1dc5f008defad77edfaa9fa341982975b173
JonesJoshuaL <74150574+InterludeGameStudios@users.noreply.github.com>
2021-10-18 15:42:22 -0500

    Added logo

--- workshop-app/main/default/lwc/containerPage/containerPage.js
+++ workshop-app/main/default/lwc/containerPage/containerPage.js
@@ -1,3 +1,6 @@
 import { LightningElement } from 'lwc';
+import logo from '@salesforce/resourceUrl/MusicWorkshopLogo';
 
-export default class ContainerPage extends LightningElement {}
+export default class ContainerPage extends LightningElement {
+  logoimage = logo;
+}
